22797c069b172efcd50b1cc7ccd9bbca10953603
kauatwn <kaualopescontato@gmail.com>
2024-02-23 20:58:38 -0300

    Criando ProductsController

---
+++ src/CatalogDb.API/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogDb.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}

f3866d8fd9dbed66517146d041ee57440948380d
kauatwn <kaualopescontato@gmail.com>
2024-02-23 13:28:46 -0300

    Criando Entities

---
+++ src/CatalogDb.API/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace CatalogDb.API.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+
+        // Define relacionamento N:1
+        public List<Product>? Products { get; set; } // Category pode ter uma coleção de Product
+
+        public Category()
+        {
+            Products = [];
+        }
+    }
+}
---
+++ src/CatalogDb.API/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace CatalogDb.API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public float Stock { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        //// Não é necessário explicitar os atributos abaixo, pois o EF Core já realizará o mapeamento do relacionamento
+        //// Define relacionamento 1:N
+        //public int CategoryId { get; set; } // FK
+        //public Category? Category { get; set; } // Product está mapeado para Category
+    }
+}

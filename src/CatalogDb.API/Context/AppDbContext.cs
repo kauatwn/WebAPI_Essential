7a99db632c1503aaa43e85cbf40f6c76ae6de7d3
kauatwn <kaualopescontato@gmail.com>
2024-02-23 13:30:22 -0300

    Criando DbContext

---
+++ src/CatalogDb.API/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CatalogDb.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogDb.API.Context
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}

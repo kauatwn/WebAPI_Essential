e0e6f3cc872c7ddcbff0de69ffafc6dc6a33f685
kauatwn <kaualopescontato@gmail.com>
2024-02-23 19:10:15 -0300

    Inserindo dados nas tabelas

---
+++ src/CatalogDb.API/Migrations/20240223215649_InsertDataCategoriesTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogDb.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertDataCategoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) VALUES('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) VALUES('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) VALUES('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}

f626d26cc87f91285b9df5dd64a06eac7f61dc88
kauatwn <kaualopescontato@gmail.com>
2024-03-19 14:10:20 -0300

    Criando UnitOfWork para impedir duplicação de atualizações de dados e ordenar o CRUD

---
+++ src/CatalogDb.API/Repositories/IUnityOfWork.cs
@@ -0,0 +1,11 @@
+﻿using CatalogDb.API.Context;
+
+namespace CatalogDb.API.Repositories
+{
+    public interface IUnityOfWork
+    {
+        public IProductRepository ProductRepository { get; }
+        public ICategoryRepository CategoryRepository { get; }
+        public void Commit();
+    }
+}
---
+++ src/CatalogDb.API/Repositories/UnityOfWork.cs
@@ -0,0 +1,41 @@
+﻿using CatalogDb.API.Context;
+
+namespace CatalogDb.API.Repositories
+{
+    public class UnityOfWork(AppDbContext context) : IUnityOfWork
+    {
+        private IProductRepository? _productRepository;
+        private ICategoryRepository? _categoryRepository;
+
+        public AppDbContext Context = context;
+
+        public IProductRepository ProductRepository
+        {
+            get
+            {
+                // Null coalescing.
+                // Se _productRepository for null, recebe ProductRepository(Context).
+                // Se não, recebe _productRepository.
+                return _productRepository ??= new ProductRepository(Context);
+            }
+        }
+
+        public ICategoryRepository CategoryRepository
+        {
+            get
+            {
+                return _categoryRepository ??= new CategoryRepository(Context);
+            }
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}

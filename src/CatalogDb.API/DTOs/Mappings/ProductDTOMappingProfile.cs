f208d89723974873499eaf4abd94782da7532050
kauatwn <kaualopescontato@gmail.com>
2024-03-20 20:53:31 -0300

    Criando ProductDTO e mapeando com AutoMapper

---
+++ src/CatalogDb.API/DTOs/Mappings/ProductDTOMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CatalogDb.API.Entities;
+
+namespace CatalogDb.API.DTOs.Mappings
+{
+    public class ProductDTOMappingProfile : Profile
+    {
+        public ProductDTOMappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
---
+++ src/CatalogDb.API/DTOs/ProductDTO.cs
@@ -0,0 +1,4 @@
+﻿namespace CatalogDb.API.DTOs
+{
+    public record ProductDTO(int Id, string? Name, string? Description, decimal Price, string? ImageUrl, int CategoryId);
+}

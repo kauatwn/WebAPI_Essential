b290339a69f970cad29c7a644305312ebd7869f1
kauatwn <kaualopescontato@gmail.com>
2024-03-20 17:09:49 -0300

    Implementando DTOs no CategoriesController

--- src/CatalogDb.API/Controllers/CategoriesController.cs
+++ src/CatalogDb.API/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using CatalogDb.API.Entities;
+﻿using CatalogDb.API.DTOs;
+using CatalogDb.API.Entities;
+using CatalogDb.API.Extensions;
 using CatalogDb.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,48 +13,54 @@ namespace CatalogDb.API.Controllers
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
         [HttpGet]
-        public ActionResult<IEnumerable<Category>> Get()
+        public ActionResult<IEnumerable<CategoryDTO>> Get()
         {
             var categories = _unitOfWork.CategoryRepository.GetCategories();
-            return Ok(categories);
+            var categoriesDto = categories.ToCategoriesDTOList();
+            return Ok(categoriesDto);
         }
 
         [HttpGet("{id:int}", Name = "ObterCategoria")]
-        public ActionResult<Category> Get(int id)
+        public ActionResult<CategoryDTO> Get(int id)
         {
-            var categoria = _unitOfWork.CategoryRepository.GetCategory(id);
-            return Ok(categoria);
-
+            var category = _unitOfWork.CategoryRepository.GetCategory(id);
+            var categoryDto = category.ToCategoryDTO();
+            return Ok(categoryDto);
         }
 
         [HttpPost]
-        public ActionResult Post(Category category)
+        public ActionResult<CategoryDTO> Post(CategoryDTO categoryDto)
         {
+            var category = categoryDto.ToCategory();
             var createdCategory = _unitOfWork.CategoryRepository.Create(category);
             _unitOfWork.Commit();
-            return new CreatedAtRouteResult("ObterCategoria", new { id = category.Id }, createdCategory);
+            var createdCategoryDto = createdCategory.ToCategoryDTO();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = category.Id }, createdCategoryDto);
         }
 
         [HttpPut("{id:int}")]
-        public ActionResult Put(int id, Category category)
+        public ActionResult<CategoryDTO> Put(int id, CategoryDTO categoryDto)
         {
-            if (id != category.Id)
+            if (id != categoryDto.Id)
             {
                 return BadRequest("Invalid data.");
             }
 
-            _unitOfWork.CategoryRepository.Update(category);
+            var category = categoryDto.ToCategory();
+            var updatedCategory = _unitOfWork.CategoryRepository.Update(category);
             _unitOfWork.Commit();
-            return Ok(category);
+            var updatedCategoryDto = updatedCategory.ToCategoryDTO();
+            return Ok(updatedCategoryDto);
         }
 
         [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
+        public ActionResult<CategoryDTO> Delete(int id)
         {
             _unitOfWork.CategoryRepository.GetCategory(id);
             var deletedCategory = _unitOfWork.CategoryRepository.Delete(id);
             _unitOfWork.Commit();
-            return Ok(deletedCategory);
+            var deletedCategoryDto = deletedCategory.ToCategoryDTO();
+            return Ok(deletedCategoryDto);
         }
     }
 }

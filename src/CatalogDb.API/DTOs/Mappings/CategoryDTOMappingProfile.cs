15eee2608bd990bacb3d5d84ca5f591c0b5e8ae6
kauatwn <kaualopescontato@gmail.com>
2024-03-20 21:50:29 -0300

    Criando CategoryDTO e mapeando com AutoMapper

---
+++ src/CatalogDb.API/DTOs/Mappings/CategoryDTOMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CatalogDb.API.Entities;
+
+namespace CatalogDb.API.DTOs.Mappings
+{
+    public class CategoryDTOMappingProfile : Profile
+    {
+        public CategoryDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}

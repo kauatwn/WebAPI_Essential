ffb081d859791df6549f8618129ccfdd33e49468
kauatwn <kaualopescontato@gmail.com>
2024-03-23 20:43:01 -0300

    Criando classe para filtragem de produtos por preço

---
+++ src/CatalogDb.API/Pagination/ProductPriceFilter.cs
@@ -0,0 +1,8 @@
+﻿namespace CatalogDb.API.Pagination
+{
+    public sealed class ProductPriceFilter : QueryStringParameters
+    {
+        public decimal? Price { get; set; }
+        public string? PriceCriterion { get; set; }
+    }
+}

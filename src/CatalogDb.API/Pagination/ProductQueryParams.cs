6b80ac9980973c54e4153ccec9dccb13b61ac058
kauatwn <kaualopescontato@gmail.com>
2024-03-22 23:07:05 -0300

    Criando paginação de dados

---
+++ src/CatalogDb.API/Pagination/ProductQueryParams.cs
@@ -0,0 +1,15 @@
+﻿namespace CatalogDb.API.Pagination
+{
+    public class ProductQueryParams
+    {
+        private const int _maxPageSize = 10;
+        public int PageNumber { get; set; }
+        private int _pageSize;
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > _maxPageSize) ? _maxPageSize : value; }
+        }
+    }
+}

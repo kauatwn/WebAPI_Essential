01b7fbeeedc552884e4b733f057823edd881ae75
kauatwn <kaualopescontato@gmail.com>
2024-03-23 16:49:55 -0300

    Criando repositório genérico

---
+++ src/CatalogDb.API/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace CatalogDb.API.Repositories
+{
+    public interface IRepository<T>
+    {
+        public IQueryable<T> GetAll();
+        public T? Get(Expression<Func<T, bool>> predicate);
+        public T Create(T entity);
+        public T Update(T entity);
+        public T Delete(T entity);
+    }
+}
---
+++ src/CatalogDb.API/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using CatalogDb.API.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CatalogDb.API.Repositories
+{
+    public class Repository<T>(AppDbContext context) : IRepository<T> where T : class 
+    {
+        protected readonly AppDbContext _context = context;
+
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public T? Get(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().AsNoTracking().FirstOrDefault(predicate);
+        }
+
+        public T Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            return entity;
+        }
+
+        public T Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return entity;
+        }
+    }
+}

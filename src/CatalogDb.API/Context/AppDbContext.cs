768e198ffead96981390ea0feff1583cf5c6e8c5
kauatwn <kaualopescontato@gmail.com>
2024-04-21 22:36:10 -0300

    Ajuste no contexto

--- src/CatalogDb.API/Context/AppDbContext.cs
+++ src/CatalogDb.API/Context/AppDbContext.cs
@@ -4,10 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CatalogDb.API.Context
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Category>? Categories { get; set; }
-        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {

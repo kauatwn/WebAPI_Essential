9275c070b6cf85346735c05d0a2a0b47a29376c1
kauatwn <kaualopescontato@gmail.com>
2024-04-21 22:39:05 -0300

    Criando nova migration para refletir mudanças

---
+++ src/CatalogDb.API/Migrations/20240422012816_AdjustImageUrlColumnRequirement.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogDb.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustImageUrlColumnRequirement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Categories",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Categories",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}

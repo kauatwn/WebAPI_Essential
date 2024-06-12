b202999b424742c9eb2a3a43b0b3daf747ee52dd
kauatwn <kaualopescontato@gmail.com>
2024-06-12 17:50:48 -0300

    Criando filtro para obter produtos com base no critério de preço

---
+++ src/CatalogDb.API/Pagination/Filters/Products/ProductPriceCriterionFilter.cs
@@ -0,0 +1,31 @@
+﻿using CatalogDb.API.Entities;
+
+namespace CatalogDb.API.Pagination.Filters.Products
+{
+    public sealed class ProductPriceCriterionFilter : BaseFilter<Product>
+    {
+        public string? PriceCriterion { get; set; }
+
+        public override IQueryable<Product> HandleFilter(IQueryable<Product> filter)
+        {
+            if (string.IsNullOrEmpty(PriceCriterion))
+            {
+                return filter;
+            }
+
+            if (PriceCriterion.Equals("greater", StringComparison.OrdinalIgnoreCase))
+            {
+                filter = filter.OrderByDescending(p => p.Price)
+                    .ThenBy(p => p.Id);
+            }
+
+            if (PriceCriterion.Equals("less", StringComparison.OrdinalIgnoreCase))
+            {
+                filter = filter.OrderBy(p => p.Price)
+                    .ThenBy(p => p.Id);
+            }
+
+            return filter;
+        }
+    }
+}

40c41c2c3acacc6198ea26f8c3ed2f41b1461d92
kauatwn <kaualopescontato@gmail.com>
2024-06-12 17:51:42 -0300

    Implementando GetProductsFilteredByPriceCriterion

--- src/CatalogDb.API/Repositories/ProductRepository.cs
+++ src/CatalogDb.API/Repositories/ProductRepository.cs
@@ -29,15 +29,33 @@ namespace CatalogDb.API.Repositories
 
         public async Task<PagedList<Product>> GetProductsFilteredByExactPrice(ProductExactPriceFilter filter)
         {
-            IQueryable<Product> products = GetAll();
+            IOrderedQueryable<Product> orderedProducts = GetAll()
+                .OrderBy(p => p.Id);
 
-            IQueryable<Product> filteredProducts = filter.HandleFilter(products);
+            IQueryable<Product> filteredProducts = filter.HandleFilter(orderedProducts);
 
             var pagedProducts = await PagedList<Product>.ToPagedList(filteredProducts, filter.PageNumber, filter.PageSize);
 
             if (pagedProducts.Count == 0)
             {
                 throw new InvalidOperationException($"It does not exist products with the price ${filter.Price}");
+            }
+
+            return pagedProducts;
+        }
+
+        public async Task<PagedList<Product>> GetProductsFilteredByPriceCriterion(ProductPriceCriterionFilter filter)
+        {
+            IOrderedQueryable<Product> orderedProducts = GetAll()
+                .OrderBy(p => p.Id);
+
+            IQueryable<Product> filteredProducts = filter.HandleFilter(orderedProducts);
+
+            var pagedProducts = await PagedList<Product>.ToPagedList(filteredProducts, filter.PageNumber, filter.PageSize);
+
+            if (pagedProducts.Count == 0)
+            {
+                throw new InvalidOperationException("No products were found based on the given price criterion");
             }
 
             return pagedProducts;

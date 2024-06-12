d40736fa749528c89e90c0c56f19e013d55f0b80
kauatwn <kaualopescontato@gmail.com>
2024-06-11 23:01:54 -0300

    Renomeando classe abstrata e deixando-a genérica para suportar filtragem genérica

---
+++ src/CatalogDb.API/Pagination/Filters/BaseFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace CatalogDb.API.Pagination.Filters
+{
+    public abstract class BaseFilter<T> where T : class
+    {
+        private const int MaxPageSize = 10;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = MaxPageSize;
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = value > MaxPageSize ? MaxPageSize : value; }
+        }
+
+        public abstract IQueryable<T> HandleFilter(IQueryable<T> filter);
+    }
+}

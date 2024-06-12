52ee85524f80f23d0fcf6a88de86823718f2acc6
kauatwn <kaualopescontato@gmail.com>
2024-06-11 23:10:45 -0300

    Criando filtro para obter nome de uma categoria

---
+++ src/CatalogDb.API/Pagination/Filters/Categories/CategoryNameFilter.cs
@@ -0,0 +1,19 @@
+﻿using CatalogDb.API.Entities;
+
+namespace CatalogDb.API.Pagination.Filters.Categories
+{
+    public sealed class CategoryNameFilter : BaseFilter<Category>
+    {
+        public string? Name { get; set; }
+
+        public override IQueryable<Category> HandleFilter(IQueryable<Category> filter)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                filter = filter.Where(c => c.Name.Contains(Name));
+            }
+
+            return filter;
+        }
+    }
+}

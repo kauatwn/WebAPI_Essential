597691811191fa29f038f802b3c14da640c68cce
kauatwn <kaualopescontato@gmail.com>
2024-06-11 23:10:56 -0300

    Refatorando interface de ICategoryRepository para refletir mudanças nos filtros

--- src/CatalogDb.API/Repositories/ICategoryRepository.cs
+++ src/CatalogDb.API/Repositories/ICategoryRepository.cs
@@ -1,12 +1,13 @@
 ﻿using CatalogDb.API.Entities;
 using CatalogDb.API.Pagination;
+using CatalogDb.API.Pagination.Filters;
+using CatalogDb.API.Pagination.Filters.Categories;
 
 namespace CatalogDb.API.Repositories
 {
     public interface ICategoryRepository : IRepository<Category>
     {
-        Task<PagedList<Category>> GetPagedCategoriesAsync(CategoryQueryParameters query);
+        Task<PagedList<Category>> GetPagedCategoriesAsync(BaseFilter<Category> filter);
         Task<PagedList<Category>> GetCategoriesFilteredByNameAsync(CategoryNameFilter filter);
-        Task<PagedList<Category>> GetCategoriesWithProductsAsync(CategoryQueryParameters query);
     }
 }

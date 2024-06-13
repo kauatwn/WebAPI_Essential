c204e18a09e9248f3b15a37f74004cdd58c31a26
kauatwn <kaualopescontato@gmail.com>
2024-06-13 15:06:49 -0300

    Criando testes unitários para ProductExactPriceFilter

---
+++ tests/CatalogDb.API.UnitTests/Pagination/Filters/Products/ProductExactPriceFilterTests.cs
@@ -0,0 +1,52 @@
+﻿using CatalogDb.API.Entities;
+using CatalogDb.API.Pagination.Filters.Products;
+using FluentAssertions;
+
+namespace CatalogDb.API.UnitTests.Pagination.Filters.Products
+{
+    public class ProductExactPriceFilterTests
+    {
+        [Fact]
+        public void HandleFilter_ByExactPrice_ReturnsQueryFilteredProductsWhenMatching()
+        {
+            // Arrange
+            IQueryable<Product> products = new List<Product>
+            {
+                new() { Id = 1, Price = 10.0m },
+                new() { Id = 2, Price = 15.0m },
+                new() { Id = 3, Price = 10.0m },
+                new() { Id = 4, Price = 20.0m }
+            }.AsQueryable();
+
+            var filter = new ProductExactPriceFilter { Price = 10.0m };
+
+            // Act
+            IQueryable<Product> filteredProducts = filter.HandleFilter(products);
+
+            // Assert
+            filteredProducts.Should().HaveCount(2);
+            filteredProducts.Should().OnlyContain(p => p.Price == filter.Price.Value);
+        }
+
+        [Fact]
+        public void HandleFilter_ByExactPrice_ReturnsEmptyQueryWhenNoMatching()
+        {
+            // Arrange
+            IQueryable<Product> products = new List<Product>
+            {
+                new() { Id = 1, Price = 10.0m },
+                new() { Id = 2, Price = 15.0m },
+                new() { Id = 3, Price = 10.0m },
+                new() { Id = 4, Price = 20.0m }
+            }.AsQueryable();
+
+            var filter = new ProductExactPriceFilter { Price = 5.0m };
+
+            // Act
+            IQueryable<Product> filteredProducts = filter.HandleFilter(products);
+
+            // Assert
+            filteredProducts.Should().BeEmpty();
+        }
+    }
+}

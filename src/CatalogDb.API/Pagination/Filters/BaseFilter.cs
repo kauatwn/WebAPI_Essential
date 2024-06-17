01e03227acb358d7199ab3ccf1204f2f3e5ab72e
kauatwn <kaualopescontato@gmail.com>
2024-06-17 16:23:05 -0300

    Deixando BaseFilter com classe base concreta

--- src/CatalogDb.API/Pagination/Filters/BaseFilter.cs
+++ src/CatalogDb.API/Pagination/Filters/BaseFilter.cs
@@ -1,9 +1,10 @@
 ﻿namespace CatalogDb.API.Pagination.Filters
 {
-    public abstract class BaseFilter<T> where T : class
+    public class BaseFilter<T> where T : class
     {
-        private const int MaxPageSize = 10;
         public int PageNumber { get; set; } = 1;
+        private const int MaxPageSize = 10;
+
         private int _pageSize = MaxPageSize;
 
         public int PageSize
@@ -12,6 +13,9 @@
             set { _pageSize = value > MaxPageSize ? MaxPageSize : value; }
         }
 
-        public abstract IQueryable<T> HandleFilter(IQueryable<T> filter);
+        public virtual IQueryable<T> HandleFilter(IQueryable<T> filter)
+        {
+            return filter;
+        }
     }
 }

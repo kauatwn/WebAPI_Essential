b7127c7085cbf26c9e31867372613cdd3c65611a
kauatwn <kaualopescontato@gmail.com>
2024-06-17 16:25:32 -0300

    Corrigindo o uso incorreto da classe abstrata em GetPagedCategoriesAsync

--- src/CatalogDb.API/Repositories/ICategoryRepository.cs
+++ src/CatalogDb.API/Repositories/ICategoryRepository.cs
@@ -1,12 +1,13 @@
 ﻿using CatalogDb.API.Entities;
 using CatalogDb.API.Pagination;
+using CatalogDb.API.Pagination.Filters;
 using CatalogDb.API.Pagination.Filters.Categories;
 
 namespace CatalogDb.API.Repositories
 {
     public interface ICategoryRepository : IRepository<Category>
     {
-        Task<PagedList<Category>> GetPagedCategoriesAsync(int pageNumber, int pageSize);
+        Task<PagedList<Category>> GetPagedCategoriesAsync(BaseFilter<Category> filter);
         Task<PagedList<Category>> GetCategoriesFilteredByNameAsync(CategoryNameFilter filter);
     }
 }
--- src/CatalogDb.API/Repositories/IProductRepository.cs
+++ src/CatalogDb.API/Repositories/IProductRepository.cs
@@ -1,12 +1,13 @@
 ﻿using CatalogDb.API.Entities;
 using CatalogDb.API.Pagination;
+using CatalogDb.API.Pagination.Filters;
 using CatalogDb.API.Pagination.Filters.Products;
 
 namespace CatalogDb.API.Repositories
 {
     public interface IProductRepository : IRepository<Product>
     {
-        Task<PagedList<Product>> GetPagedProductsAsync(int pageNumber, int pageSize);
+        Task<PagedList<Product>> GetPagedProductsAsync(BaseFilter<Product> filter);
         Task<PagedList<Product>> GetProductsFilteredByExactPriceAsync(ProductExactPriceFilter filter);
         Task<PagedList<Product>> GetProductsFilteredByPriceCriterionAsync(ProductPriceCriterionFilter filter);
         Task<PagedList<Product>> GetProductsFilteredByPriceWithCriterionAsync(ProductPriceWithCriterionFilter filter);

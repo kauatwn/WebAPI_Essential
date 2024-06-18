63f1e978cfc8dcada404089fd41a8cf69dbc6aa2
kauatwn <kaualopescontato@gmail.com>
2024-06-18 20:33:45 -0300

    Refatorando filtro de ordenação por critério

---
+++ src/CatalogDb.API/Pagination/Filters/Products/ProductPriceOrderFilter.cs
@@ -0,0 +1,26 @@
+﻿using CatalogDb.API.Entities;
+
+namespace CatalogDb.API.Pagination.Filters.Products
+{
+    public sealed class ProductPriceOrderFilter : BaseFilter<Product>
+    {
+        public string? Criterion { get; set; }
+
+        public override IQueryable<Product> HandleFilter(IQueryable<Product> filter)
+        {
+            if (!string.IsNullOrEmpty(Criterion))
+            {
+                IQueryable<Product> orderedByCriterion = Criterion.ToLower() switch
+                {
+                    "greater" => filter.OrderByDescending(p => p.Price).ThenBy(p => p.Id),
+                    "less" => filter.OrderBy(p => p.Price).ThenBy(p => p.Id),
+                    _ => filter
+                };
+
+                return orderedByCriterion;
+            }
+
+            return base.HandleFilter(filter);
+        }
+    }
+}

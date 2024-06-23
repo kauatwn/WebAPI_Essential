97ae0b4cd545258ec143247c43053b1d5ff909ab
kauatwn <kaualopescontato@gmail.com>
2024-06-23 17:25:42 -0300

    Criando exceção personalizada para recurso não encontrado

---
+++ src/CatalogDb.API/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace CatalogDb.API.Exceptions
+{
+    public class ResourceNotFoundException : Exception
+    {
+        public ResourceNotFoundException()
+        {
+        }
+        public ResourceNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}

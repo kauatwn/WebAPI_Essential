7ae185b205c184ce349a613b8d3834f67eae80b8
kauatwn <kaualopescontato@gmail.com>
2024-06-23 17:31:25 -0300

    Movendo lógica de ProductsController para a camada de serviço

--- src/CatalogDb.API/Controllers/ProductsController.cs
+++ src/CatalogDb.API/Controllers/ProductsController.cs
@@ -1,10 +1,8 @@
-﻿using AutoMapper;
-using CatalogDb.API.DTOs;
+﻿using CatalogDb.API.DTOs;
 using CatalogDb.API.Entities;
-using CatalogDb.API.Pagination;
 using CatalogDb.API.Pagination.Filters;
 using CatalogDb.API.Pagination.Filters.Products;
-using CatalogDb.API.Repositories;
+using CatalogDb.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -14,34 +12,29 @@ namespace CatalogDb.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private IUnitOfWork<Product> UnitOfWork { get; }
-        private IMapper Mapper { get; }
+        private IProductService ProductService { get; }
 
-        public ProductsController(IUnitOfWork<Product> unitOfWork, IMapper mapper)
+        public ProductsController(IProductService productService)
         {
-            UnitOfWork = unitOfWork;
-            Mapper = mapper;
+            ProductService = productService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts([FromQuery] BaseFilter<Product> filter)
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts([FromQuery] PaginationFilter<Product> source)
         {
-            PagedList<Product> products = await UnitOfWork.ProductRepository.GetPagedProductsAsync(filter);
+            PaginationResultDTO<ProductDTO> result = await ProductService.GetPagedProductsAsync(source);
 
-            return GenerateResponse(products);
+            // Cabeçalho que será enviado ao front contendo informações de paginação.
+            // Número da página atual, tamanho da página, total de itens, etc.
+            Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(result.Metadata));
+
+            return Ok(result.Items);
         }
 
         [HttpGet("{id:int}", Name = nameof(GetProductById))]
         public async Task<ActionResult<ProductDTO>> GetProductById(int id)
         {
-            Product? product = await UnitOfWork.Repository.GetByIdAsync(p => p.Id == id);
-
-            if (product == null)
-            {
-                return NotFound($"No product found with the ID {id}");
-            }
-
-            ProductDTO productDto = Mapper.Map<ProductDTO>(product);
+            ProductDTO productDto = await ProductService.GetProductByIdAsync(id);
 
             return Ok(productDto);
         }
@@ -49,117 +42,52 @@ namespace CatalogDb.API.Controllers
         [HttpGet("Filter/ExactPrice")]
         public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProductsFilteredByExactPrice([FromQuery] ProductExactPriceFilter filter)
         {
-            PagedList<Product> products = await UnitOfWork.ProductRepository.GetProductsFilteredByExactPriceAsync(filter);
+            PaginationResultDTO<ProductDTO> result = await ProductService.GetProductsByExactPriceAsync(filter);
+            Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(result.Metadata));
 
-            if (products.Count == 0)
-            {
-                return NotFound("No products found with the specified exact price");
-            }
-
-            return GenerateResponse(products);
+            return Ok(result.Items);
         }
 
         [HttpGet("Filter/PriceCriterion")]
         public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProductsFilteredByPriceCriterion([FromQuery] ProductPriceOrderFilter filter)
         {
-            PagedList<Product> products = await UnitOfWork.ProductRepository.GetProductsFilteredByPriceCriterionAsync(filter);
+            PaginationResultDTO<ProductDTO> result = await ProductService.GetProductsByPriceCriterionAsync(filter);
+            Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(result.Metadata));
 
-            if (products.Count == 0)
-            {
-                return NotFound("No products found with the specified price criterion");
-            }
-
-            return GenerateResponse(products);
+            return Ok(result.Items);
         }
 
         [HttpGet("Filter/PriceAndAdditionalCriterion")]
-        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProductsFilteredByPriceWithCriterion([FromQuery] ProductAdvancedPriceFilter filter)
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProductsByAdvancedPriceFilter([FromQuery] ProductAdvancedPriceFilter filter)
         {
-            PagedList<Product> products = await UnitOfWork.ProductRepository.GetProductsFilteredByPriceWithCriterionAsync(filter);
+            PaginationResultDTO<ProductDTO> result = await ProductService.GetProductsByAdvancedPriceFilterAsync(filter);
+            Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(result.Metadata));
 
-            if (products.Count == 0)
-            {
-                return NotFound("No products found with the specified price and additional criterion");
-            }
-
-            return GenerateResponse(products);
+            return Ok(result.Items);
         }
-
 
         [HttpPost]
         public async Task<ActionResult<ProductDTO>> Post(ProductDTO productDto)
         {
-            if (productDto == null)
-            {
-                return BadRequest("Invalid data received: Product data is null");
-            }
+            ProductDTO result = await ProductService.CreateProductAsync(productDto);
 
-            Product product = Mapper.Map<Product>(productDto);
-            Product createdProduct = UnitOfWork.Repository.Create(product);
-
-            await UnitOfWork.CommitAsync();
-
-            ProductDTO createdProductDto = Mapper.Map<ProductDTO>(createdProduct);
-
-            return CreatedAtRoute(nameof(GetProductById), new { id = product.Id }, createdProductDto);
+            return CreatedAtRoute(nameof(GetProductById), new { id = result.Id }, result);
         }
 
         [HttpPut("{id:int}")]
         public async Task<ActionResult<ProductDTO>> Put(int id, ProductDTO productDto)
         {
-            if (id != productDto.Id || productDto == null)
-            {
-                return BadRequest("Invalid data received: Product ID mismatch or data is null");
-            }
+            ProductDTO result = await ProductService.UpdateProductAsync(id, productDto);
 
-            Product product = Mapper.Map<Product>(productDto);
-            Product updatedProduct = UnitOfWork.Repository.Update(product);
-
-            await UnitOfWork.CommitAsync();
-
-            ProductDTO updatedProductDto = Mapper.Map<ProductDTO>(updatedProduct);
-
-            return Ok(updatedProductDto);
+            return Ok(result);
         }
 
         [HttpDelete("{id:int}")]
         public async Task<ActionResult<ProductDTO>> Delete(int id)
         {
-            Product? product = await UnitOfWork.Repository.GetByIdAsync(p => p.Id == id);
+            ProductDTO result = await ProductService.DeleteProductAsync(id);
 
-            if (product == null)
-            {
-                return NotFound($"No product found with the ID {id}");
-            }
-
-            Product deletedProduct = UnitOfWork.Repository.Delete(product);
-
-            await UnitOfWork.CommitAsync();
-
-            ProductDTO deletedProductDto = Mapper.Map<ProductDTO>(deletedProduct);
-
-            return Ok(deletedProductDto);
-        }
-
-        private ActionResult<IEnumerable<ProductDTO>> GenerateResponse(PagedList<Product> products)
-        {
-            object metadata = new
-            {
-                products.TotalCount,
-                products.PageSize,
-                products.CurrentPage,
-                products.TotalPages,
-                products.HasPreviousPage,
-                products.HasNextPage
-            };
-
-            // Cabeçalho que será enviado ao front contendo informações de paginação.
-            // Número da página atual, tamanho da página, total de itens, etc.
-            Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata));
-
-            IEnumerable<ProductDTO> productsDto = Mapper.Map<IEnumerable<ProductDTO>>(products);
-
-            return Ok(productsDto);
+            return Ok(result);
         }
     }
 }

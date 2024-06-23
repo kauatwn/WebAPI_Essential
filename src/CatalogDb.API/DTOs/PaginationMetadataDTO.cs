a4bb899b6f8d79a6c4bea6e692813b1b40f5fed3
kauatwn <kaualopescontato@gmail.com>
2024-06-23 17:24:40 -0300

    Criando classe para obter metadados de paginação

---
+++ src/CatalogDb.API/DTOs/PaginationMetadataDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace CatalogDb.API.DTOs
+{
+    public record PaginationMetadataDTO
+    {
+        public int CurrentPage { get; init; }
+        public int TotalPages { get; init; }
+        public int PageSize { get; init; }
+        public int TotalCount { get; init; }
+
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
+
+        public PaginationMetadataDTO(int totalCount, int pageNumber, int pageSize)
+        {
+            TotalCount = totalCount;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+        }
+    }
+}

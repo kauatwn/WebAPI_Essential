615b84da6102cde3382704caa5313838a0da69e0
kauatwn <kaualopescontato@gmail.com>
2024-06-23 17:27:26 -0300

    Criando interface de serviço para Category

---
+++ src/CatalogDb.API/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using CatalogDb.API.DTOs;
+using CatalogDb.API.Entities;
+using CatalogDb.API.Pagination.Filters;
+using CatalogDb.API.Pagination.Filters.Categories;
+
+namespace CatalogDb.API.Services
+{
+    public interface ICategoryService
+    {
+        Task<PaginationResultDTO<CategoryDTO>> GetPagedCategoriesAsync(PaginationFilter<Category> source);
+        Task<CategoryDTO> GetCategoryByIdAsync(int id);
+        Task<PaginationResultDTO<CategoryDTO>> GetCategoriesByName(CategoryNameFilter source);
+        Task<CategoryDTO> CreateCategoryAsync(CategoryDTO categoryDto);
+        Task<CategoryDTO> UpdateCategoryAsync(int id, CategoryDTO categoryDto);
+        Task<CategoryDTO> DeleteCategoryAsync(int id);
+    }
+}

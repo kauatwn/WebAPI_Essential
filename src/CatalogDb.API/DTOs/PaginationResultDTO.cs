1e5d67cf0864e0f56111baa75e6c15d0b9e1b7ac
kauatwn <kaualopescontato@gmail.com>
2024-06-23 17:25:10 -0300

    Criando classe para obter lista de itens e metadados de paginação

---
+++ src/CatalogDb.API/DTOs/PaginationResultDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogDb.API.DTOs
+{
+    public record PaginationResultDTO<T> where T : class
+    {
+        public IEnumerable<T> Items { get; init; }
+        public PaginationMetadataDTO Metadata { get; init; }
+
+        public PaginationResultDTO(IEnumerable<T> items, PaginationMetadataDTO metadata)
+        {
+            Items = items;
+            Metadata = metadata;
+        }
+    }
+}

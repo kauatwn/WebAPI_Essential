28975f788e5901e4460bb3bf2eb3c6ebd655d135
kauatwn <kaualopescontato@gmail.com>
2024-06-23 17:18:53 -0300

    Criando classe genérica de paginação

---
+++ src/CatalogDb.API/Pagination/Filters/PaginationFilter.cs
@@ -0,0 +1,26 @@
+﻿namespace CatalogDb.API.Pagination.Filters
+{
+    public class PaginationFilter<T> where T : class
+    {
+        private const int MaxPageSize = 10;
+        private int _pageSize = MaxPageSize;
+        private int _pageNumber = 1;
+
+        public int PageNumber
+        {
+            get { return _pageNumber; }
+            set { _pageNumber = value < 1 ? 1 : value; }
+        }
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = value > MaxPageSize ? MaxPageSize : value; }
+        }
+
+        public virtual IQueryable<T> HandleFilter(IQueryable<T> source)
+        {
+            return source.Skip((PageNumber - 1) * PageSize).Take(PageSize);
+        }
+    }
+}

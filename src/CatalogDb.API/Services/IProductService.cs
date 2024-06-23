4a4a3a382590df6e612188c53a4a535abeb668df
kauatwn <kaualopescontato@gmail.com>
2024-06-23 17:26:52 -0300

    Criando interface de serviço para Product

---
+++ src/CatalogDb.API/Services/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using CatalogDb.API.DTOs;
+using CatalogDb.API.Entities;
+using CatalogDb.API.Pagination.Filters;
+using CatalogDb.API.Pagination.Filters.Products;
+
+namespace CatalogDb.API.Services
+{
+    public interface IProductService
+    {
+        Task<PaginationResultDTO<ProductDTO>> GetPagedProductsAsync(PaginationFilter<Product> source);
+        Task<ProductDTO> GetProductByIdAsync(int id);
+        Task<PaginationResultDTO<ProductDTO>> GetProductsByExactPriceAsync(ProductExactPriceFilter filter);
+        Task<PaginationResultDTO<ProductDTO>> GetProductsByPriceCriterionAsync(ProductPriceOrderFilter filter);
+        Task<PaginationResultDTO<ProductDTO>> GetProductsByAdvancedPriceFilterAsync(ProductAdvancedPriceFilter filter);
+        Task<ProductDTO> CreateProductAsync(ProductDTO productDto);
+        Task<ProductDTO> UpdateProductAsync(int id, ProductDTO productDto);
+        Task<ProductDTO> DeleteProductAsync(int id);
+    }
+}
